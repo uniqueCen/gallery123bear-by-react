fcfbdfce0ecd3eb8c99449ea1f00e8e733dd0508
123bear <finalcen@163.com>
2018-01-09 00:09:46 +0800

    build stage

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,28 +3,53 @@ require('styles/App.css');
 
 import React from 'react';
 
-var imageDatas = require('../data/imageDatas.json');
+var imageDatas = require('json!../data/imageDatas.json');
 
 function genImageURL(imageDatasArr){
 	for(var i = 0,j = imageDatasArr.length;i < j; i++){
-		imageDatasArr[i].imageURL = require('../images/'+imageDatasArr[i].fileName);
+		var singleImgData = imageDatasArr[i];
+		singleImgData.imageURL = require('../images/'+ singleImgData.fileName);
+		imageDatasArr[i] = singleImgData;
 	}
 	return imageDatasArr;
 }
 imageDatas = genImageURL(imageDatas);
 
-var Gallery123bearByReactApp = React.createClass({
+var ImgFigure = React.createClass({
 	render: function(){
 		return (
-			<section classsName="stage">
-				<section classsName="img-sec">
-				</section>
-				<nav classsName="controller-nav">
-				</nav>
-			</section>
+			<figure className="img-figure">
+				<img src={this.props.data.imageURL}
+					alt={this.props.data.title}
+				/>
+				<figcaption>
+					<h2>{this.props.data.title}</h2>
+				</figcaption>
+			</figure>
 		);
+	}
+});
+var Gallery123bearByReactApp = React.createClass({
+	render: function(){
+		var controllerUnits = [],
+			 imgFigures = [];
+
+		imageDatas.forEach(function(value){
+			imgFigures.push(<ImgFigure data={value} />);
+		}.bind(this));
+		return (
+	        <section className="stage" ref="stage">
+	            <section className="img-sec">
+	            	{imgFigures}
+	            </section>
+	            <nav className="controller-nav">
+	            </nav>
+	        </section>
+	    );
 	}
 
 });
 
-React.render(<Gallery123bearByReactApp/>,document.getElementById('content'));
+
+
+module.exports = Gallery123bearByReactApp;
